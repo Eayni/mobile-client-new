5e17349eaa1d256835d87889bba60e3706c2d7fe
Ahmed Khaled <ahmedkhaled4d@gmail.com>
2025-03-16 03:54:05 +0200

    tintColorLight

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect } from "react";
 import {
   DarkTheme,
   DefaultTheme,
@@ -7,7 +8,6 @@ import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
-import { useEffect } from "react";
 import "react-native-reanimated";
 
 import { useColorScheme } from "@/hooks/useColorScheme";
